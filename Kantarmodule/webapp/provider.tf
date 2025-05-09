d250f135c5fe5be53c5b559c4dd6485531960652
srinivas <srinivashyd472@gmail.com>
2025-05-09 14:33:54 +0000

    module

--- Kantarmodule/webapp/provider.tf
+++ Kantarmodule/webapp/provider.tf
@@ -9,14 +9,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-    subscription_id = "18ff0f58-399e-4529-b184-280081797dcf"
-    tenant_id       = "83fa5d54-07ff-44ba-aef3-2e35bfb88e69"
-    client_id       = "de3937ab-d39d-429a-a88c-8dbe749cfa12"
-    client_secret   = "pin8Q~Ig2q1R5EayNZ1Rg1gDQB9s~8v1LcShhb_5"
-#   subscription_id = "6d2ce6ad-24ad-413a-b41f-d06eabf65641"
-#   tenant_id       = "a4443515-8051-4627-b9d3-08b16975aeeb"
-#   client_id       = "98f81f5a-8a14-4a5f-a145-5a11e86ff91e"
-#   client_secret   = "q9q8Q~bfkDwoji1xi3rd3Fk2zZ~S0VhP2C0tSbTc"
+    subscription_id = ""
 }
 
 # if you need Backend
